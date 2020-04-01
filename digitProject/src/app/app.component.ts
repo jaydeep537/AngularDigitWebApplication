2b4f9b7ab276df164258c6b7586dfff22aa79000
jaydeep11 <solankijaydeep424@gmail.com>
2020-04-02 05:00:56 +0530

    Validation Changes

---
+++ digitProject/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  digitForm: FormGroup;
+  submitted = false;
+  digitInput = "";
+  pageOfItems = [];
+  config: any;
+constructor(private route: ActivatedRoute, private router: Router ,private http: HttpClient, private formBuilder: FormBuilder) {
+  this.config = {
+  currentPage: 1,
+  itemsPerPage: 10,
+  totalItems:0
+  };
+}
+  ngOnInit() {
+    this.digitForm = this.formBuilder.group({
+        digitText: ['', [Validators.required, Validators.minLength(7),Validators.maxLength(10)]],        
+    })
+}
+get f() { console.log("F called"); return this.digitForm.controls; }
+
+onSubmit() {
+    console.log("On Submit Called",this.digitForm.invalid , this.digitInput);
+    this.submitted = true;
+    if (this.digitForm.invalid) {
+      return;
+    }
+    // get page of items from api
+    this.http.get<any>(`https://hobs-lite-eiot-uat.westindia.cloudapp.azure.com/FindPossibleAlphabet/FindPossibleAlphabet?number=${this.digitInput}&recordperpage=5`).subscribe(x => {
+      console.log("Response",x);
+      this.buildPager(x)
+    });
+  }
+  pageChange(newPage: number) {
+    console.log("Page change called",newPage);
+          this.config.currentPage = newPage; 
+          this.http.get<any>(`https://hobs-lite-eiot-uat.westindia.cloudapp.azure.com/FindPossibleAlphabet/FindPossibleAlphabet?number=${newPage}&recordperpage=5`).subscribe(x => {
+          console.log("Response",x);
+          this.buildPager(x)
+        });
+    }
+      buildPager(items){
+      console.log(items)
+      console.log("totalItems,",this.config.totalItems);
+      this.pageOfItems = items.data.combinationList;
+      this.config.totalItems = items.data.totalNumberOfRecord;
+    }
+}
