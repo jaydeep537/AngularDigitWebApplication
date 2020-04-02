c07b35943bbcfb314a1a2e08b87192f1fa2c0314
jaydeep11 <solankijaydeep424@gmail.com>
2020-04-02 13:21:12 +0530

    per page changes

--- digitProject/src/app/app.component.ts
+++ digitProject/src/app/app.component.ts
@@ -11,14 +11,17 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class AppComponent {
   digitForm: FormGroup;
   dataFetchSuccess = false;
+  searchedInput = "";
   submitted = false;
   digitInput = "";
   pageOfItems = [];
   config: any;
+  paginationPerpageArray = [5,10,15,20];
+
 constructor(private route: ActivatedRoute, private router: Router ,private http: HttpClient, private formBuilder: FormBuilder) {
   this.config = {
   currentPage: 1,
-  itemsPerPage: 10,
+  itemsPerPage: 5,
   totalItems:0
   };
 }
@@ -35,8 +38,10 @@ onSubmit() {
     if (this.digitForm.invalid) {
       return;
     }
+    this.searchedInput = this.digitInput;
     // get page of items from api
-    this.http.get<any>(`https://hobs-lite-eiot-uat.westindia.cloudapp.azure.com/FindPossibleAlphabet/FindPossibleAlphabet?number=${this.digitInput}&recordperpage=5`).subscribe(x => {
+    this.config.currentPage = 1; 
+    this.http.get<any>(`https://hobs-lite-eiot-uat.westindia.cloudapp.azure.com/FindPossibleAlphabet/FindPossibleAlphabet?number=${this.digitInput}&recordperpage=${this.config.itemsPerPage}`).subscribe(x => {
       console.log("Response",x);
       this.buildPager(x)
     });
@@ -44,7 +49,7 @@ onSubmit() {
   pageChange(newPage: number) {
     console.log("Page change called",newPage);
           this.config.currentPage = newPage; 
-          this.http.get<any>(`https://hobs-lite-eiot-uat.westindia.cloudapp.azure.com/FindPossibleAlphabet/FindPossibleAlphabet?number=${newPage}&recordperpage=5`).subscribe(x => {
+          this.http.get<any>(`https://hobs-lite-eiot-uat.westindia.cloudapp.azure.com/FindPossibleAlphabet/FindPossibleAlphabet?number=${this.config.currentPage}&recordperpage=${this.config.itemsPerPage}`).subscribe(x => {
           console.log("Response",x);
           this.buildPager(x)
         });
@@ -59,5 +64,10 @@ onSubmit() {
       }else{
         this.dataFetchSuccess = false;
       }
+    }
+    perPageChange(pageNum){
+      console.log("Per page change",pageNum);
+      this.config.itemsPerPage = pageNum;
+      this.pageChange(this.config.currentPage);
     }
 }
